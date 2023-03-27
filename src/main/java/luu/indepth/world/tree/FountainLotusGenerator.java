be603abfcc076ac989aad0cb4f46c6ebfbbbf9b0
DancerVlt69 <dancervlt69@googlemail.com>
2023-03-27 20:19:55 +0200

    changes: moved into the world/tree folder

--- src/main/java/luu/indepth/world/tree/FountainLotusGenerator.java
+++ src/main/java/luu/indepth/world/tree/FountainLotusGenerator.java
@@ -7,10 +7,10 @@ import net.minecraft.util.math.random.Random;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import org.jetbrains.annotations.Nullable;
 
-public class PantherCapSporeGenerator extends SaplingGenerator {
+public class FountainLotusGenerator extends SaplingGenerator {
 
 	@Override
 	protected @Nullable RegistryKey<ConfiguredFeature<?, ?>> getTreeFeature(Random random, boolean bees) {
-		return ModConfiguredFeatures.PANTHER_CAP_KEY;
+		return ModConfiguredFeatures.FOUNTAIN_KEY;
 	}
 }
