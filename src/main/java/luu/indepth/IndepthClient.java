49ed2d67605f075ba39cfae5a03fd50970c9a146
DancerVlt69 <dancervlt69@googlemail.com>
2023-03-27 19:33:34 +0200

    moved BlockRenderer into (a) separated class(es)

--- src/main/java/luu/indepth/IndepthClient.java
+++ src/main/java/luu/indepth/IndepthClient.java
@@ -1,22 +1,14 @@
 package luu.indepth;
-import luu.indepth.Registry.ModBlocks;
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.minecraft.client.render.RenderLayer;
 
 import net.fabricmc.api.ClientModInitializer;
+
+import static luu.indepth.utils.renderer.ModBlockRenderer.renderModBlocks;
 
 public class IndepthClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        /*
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.COPPER_TRAPDOOR, RenderLayer.getCutout());
-        */
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PANTHER_SPORE, RenderLayer.getCutout());
 
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.FOUNTAIN_LEAF, RenderLayer.getCutout());
-//        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.FOUNTAIN_LOTUS, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.PANTHER_CAP, RenderLayer.getCutout());
+		renderModBlocks();
 
     }
 }
