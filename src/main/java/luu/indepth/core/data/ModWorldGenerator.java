d38d66a9bb55748101868c17cdde1e021602a68a
DancerVlt69 <dancervlt69@googlemail.com>
2023-03-27 21:10:17 +0200

    changes: removed unused imports

--- src/main/java/luu/indepth/core/data/ModWorldGenerator.java
+++ src/main/java/luu/indepth/core/data/ModWorldGenerator.java
@@ -1,6 +1,5 @@
 package luu.indepth.core.data;
 
-import luu.indepth.Indepth;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
 import net.minecraft.registry.RegistryKeys;
