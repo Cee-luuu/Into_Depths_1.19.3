99e5e280b48b495feb0fff0a2a7d1a681ee86987
DancerVlt69 <dancervlt69@googlemail.com>
2023-03-27 20:35:04 +0200

    changes: simplified 'switch' usage and imports | added 'HORIZONTAL_FAVING' properties (test) and the needed method

--- src/main/java/luu/indepth/block/custom/PantherTrunk.java
+++ src/main/java/luu/indepth/block/custom/PantherTrunk.java
@@ -1,15 +1,14 @@
 package luu.indepth.block.custom;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
+import net.minecraft.block.*;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.BlockRotation;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.RotationAxis;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 
@@ -18,8 +17,7 @@ public class PantherTrunk extends PillarBlock {
     protected static final VoxelShape Y_SHAPE = Block.createCuboidShape(5.0, 0.0, 5.0, 11.0, 16.0, 11.0);
     protected static final VoxelShape Z_SHAPE = Block.createCuboidShape(5.0, 5.0, 0.0, 11.0, 11.0, 16.0);
     protected static final VoxelShape X_SHAPE = Block.createCuboidShape(0.0, 5.0, 5.0, 16.0, 11.0, 11.0);
-
-
+	public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
     public static final EnumProperty<Direction.Axis> AXIS = Properties.AXIS;
 
     public PantherTrunk(AbstractBlock.Settings settings) {
@@ -31,29 +29,18 @@ public class PantherTrunk extends PillarBlock {
     public BlockState rotate(BlockState state, BlockRotation rotation) { return PantherTrunk.changeRotation(state, rotation); }
 
     public static BlockState changeRotation(BlockState state, BlockRotation rotation) {
-        switch (rotation) {
-            case COUNTERCLOCKWISE_90:
-            case CLOCKWISE_90: {
-                switch (state.get(AXIS)) {
-                    case X: {
-                        return state.with(AXIS, Direction.Axis.Z);
-                    }
-                    case Z: {
-                        return state.with(AXIS, Direction.Axis.X);
-                    }
-                    }
-                return state.with(AXIS, Direction.Axis.Y);
-            }
-        }
-        return state;
+		return switch (rotation) {
+			case COUNTERCLOCKWISE_90, CLOCKWISE_90 -> switch (state.get(AXIS)) {
+				case X -> state.with(AXIS, Direction.Axis.Z);
+				case Z -> state.with(AXIS, Direction.Axis.X);
+				default -> state;
+			};
+			default -> state;
+		};
     }
-
-
 
     @Override
-    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(AXIS);
-    }
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) { builder.add(AXIS, FACING); }
 
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
