c749208176c56a1ba32851e7be61bdfb4fff91fc
DancerVlt69 <dancervlt69@googlemail.com>
2023-03-28 05:43:04 +0200

    fixed: Lambda-Expressions

--- src/main/java/luu/indepth/utils/materials/ModArmorMaterials.java
+++ src/main/java/luu/indepth/utils/materials/ModArmorMaterials.java
@@ -12,27 +12,28 @@ import net.minecraft.util.Lazy;
 import java.util.function.Supplier;
 
 public enum ModArmorMaterials implements ArmorMaterial {
-    IRON("iron", 15, new int[]{2, 5, 6, 2}, 9, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, 0.0F, () -> {
-        return Ingredient.ofItems(Items.IRON_INGOT);
-    }),
+    IRON("iron", 15, new int[]{2, 5, 6, 2}, 9,
+			SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F, 0.0F,
+			() -> Ingredient.ofItems(Items.IRON_INGOT)),
 
-    COPPER("copper", 15, new int[]{1, 4, 5, 2}, 12, SoundEvents.ITEM_ARMOR_EQUIP_CHAIN, 0.0f, 0.0f, () -> Ingredient.ofItems(Items.COPPER_INGOT)),
+    COPPER("copper", 15, new int[]{1, 4, 5, 2}, 12,
+			SoundEvents.ITEM_ARMOR_EQUIP_CHAIN, 0.0f, 0.0f,
+			() -> Ingredient.ofItems(Items.COPPER_INGOT)),
 
+    SELENITE("selenite", 7, new int[]{1, 3, 5, 2}, 25,
+			SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 0.0F, 0.0F,
+			() -> Ingredient.ofItems(Items.GOLD_INGOT)),
 
-    SELENITE("selenite", 7, new int[]{1, 3, 5, 2}, 25, SoundEvents.ITEM_ARMOR_EQUIP_GOLD, 0.0F, 0.0F, () -> {
-        return Ingredient.ofItems(Items.GOLD_INGOT);
-    }),
-    DIAMOND("diamond", 33, new int[]{3, 6, 8, 3}, 10, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.0F, 0.0F, () -> {
-        return Ingredient.ofItems(Items.DIAMOND);
-    }),
+    DIAMOND("diamond", 33, new int[]{3, 6, 8, 3}, 10,
+			SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.0F, 0.0F,
+			() -> Ingredient.ofItems(Items.DIAMOND)),
 
-    NICKEL("nickel", 40, new int[]{3, 5, 7, 2}, 10, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.0F, 0.0F, () -> {
-        return Ingredient.ofItems(ModItems.NICKEL_INGOT);
-    }),
+    NICKEL("nickel", 40, new int[]{3, 5, 7, 2}, 10,
+			SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 2.0F, 0.0F, () -> Ingredient.ofItems(ModItems.NICKEL_INGOT)),
 
-    NETHERITE("netherite", 37, new int[]{3, 6, 8, 3}, 15, SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE, 3.0F, 0.1F, () -> {
-        return Ingredient.ofItems(Items.NETHERITE_INGOT);
-    });
+    NETHERITE("netherite", 37, new int[]{3, 6, 8, 3}, 15,
+			SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE, 3.0F, 0.1F,
+			() -> Ingredient.ofItems(Items.NETHERITE_INGOT));
 
     private static final int[] BASE_DURABILITY = new int[]{13, 15, 16, 11};
     private final String name;
