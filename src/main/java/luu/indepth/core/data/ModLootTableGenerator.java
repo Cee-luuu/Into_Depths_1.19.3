53731f6d21b455a7e2ba13ad7231b8eb9ad7defe
DancerVlt69 <dancervlt69@googlemail.com>
2023-03-28 05:39:06 +0200

    changes: removed the 'snow_campfire'

--- src/main/java/luu/indepth/core/data/ModLootTableGenerator.java
+++ src/main/java/luu/indepth/core/data/ModLootTableGenerator.java
@@ -6,16 +6,13 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
 
 public class ModLootTableGenerator extends FabricBlockLootTableProvider {
-	public ModLootTableGenerator(FabricDataOutput dataOutput) {
-		super(dataOutput);
-	}
+	public ModLootTableGenerator(FabricDataOutput dataOutput) { super(dataOutput); }
 
 	@Override
 	public void generate() {
 		// addDrop(ModBlocks.PANTHER_LOG);
 
 		addDrop(ModBlocks.MAGMA_BLACKSTONE, ModItems.FIRD);
-		addDrop(ModBlocks.SNOW_CAMPFIRE);
 
 	}
 }
